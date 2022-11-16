4d0da5fdb4283c1dc9713b5a35aaa83a02fef2c1
mynamenick <50175290+mynamenick@users.noreply.github.com>
2022-11-16 12:15:52 -0600

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -38,7 +38,7 @@
                 <img :src="require('@/assets/stocks.jpg')" class="card-img-top" style="height: 15rem">
                 <div class="card-body">
                   <h5 class="card-title">Stocks</h5>
-                  <a @click="" class="btn btn-primary">View Details</a>
+                  <a @click="viewStocks" class="btn btn-primary">View Details</a>
                 </div>
               </div>
             </div>
@@ -68,6 +68,11 @@ import router from '../router'
 
       viewInvestments() {
         router.push('/investment-list');
+      },
+
+
+      viewStocks() {
+        router.push('/stock-list');
       },
 
 
